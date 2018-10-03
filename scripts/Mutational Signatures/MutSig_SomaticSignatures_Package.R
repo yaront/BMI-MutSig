8370e79d2a900348615ff4b7be8b530ceb57cf62
yaront <ymtomer@gmail.com>
2018-10-02 23:23:32 -0400

    Mutational signature using SomaticSignatures
    R script using the SomaticSignatures package for deciphering mutational signatures

---
+++ scripts/Mutational Signatures/MutSig_SomaticSignatures_Package.R
@@ -0,0 +1,7 @@
+## ================================ ##
+##       Mutational Signatures      ##
+##     SomaticSignatures Package    ##
+## ================================ ##
+
+setwd("~/Google Drive/PhD/Thesis/Mutation Signature/scripts/") #Set Directory
+
