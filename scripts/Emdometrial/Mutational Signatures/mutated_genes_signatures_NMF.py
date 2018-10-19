560280b6f7f29de6c18ca1962e1dcdefc958c6f2
yaront <ymtomer@gmail.com>
2018-10-18 20:50:19 -0400

    NMF for mutated genes
    Taking the quintiles and finding the NMF for the mutated genes accoding to a cutoff (5% or 10% of the patients)

--- scripts/Emdometrial/Mutational Signatures/mutated_genes_signatures_NMF.py
+++ scripts/Emdometrial/Mutational Signatures/mutated_genes_signatures_NMF.py
@@ -13,8 +13,9 @@ Created on Tue Oct 16 08:27:36 2018
 
 import numpy as np
 import pandas as pd
-from sklearn.decomposition import NMF
+from sklearn.decomposition import PCA,NMF
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.cm as cm
 
 #%% Reading the data
@@ -25,28 +26,26 @@ mut_gene_mat = np.asmatrix(gene_bmi_mut.iloc[2:,:-1])
 
 #%% Dimension reduction
 
-#percent_remove = 0
-#
-#mut_freq = mut_cat.sum(axis=1)/mut_cat.sum().sum()
-#mut_freq = mut_freq.sort_values()
-#
-#low_mut_type = list(mut_freq[np.cumsum(mut_freq) < percent_remove].index)
-#mut_cat_high = mut_cat.drop(low_mut_type)
-#mut_cat_high_mat = np.asmatrix(mut_cat_high)
+percent_remove = 0.1
+
+mut_freq = mut_gene_mat.sum(axis=1)/float(mut_gene_mat.shape[1])
+
+gene_bmi_mut_high = gene_bmi_mut.iloc[2:,:-1][mut_freq>percent_remove]
+mut_gene_high_mat = np.asmatrix(gene_bmi_mut_high)
 
 
 #%% Bootstrap
 
 
 
-n_comp = 4
-row_plt = 4
-col_plt = 1
+n_comp = 5
+row_plt = 3
+col_plt = 2
 
-X = np.copy(mut_gene_mat)
-#X = mut_cat_high_mat[:,range(50) + range(190,240)]
-#X = mut_cat_high_mat.astype(np.float) / mut_cat_high_mat.sum(axis=0)
-#X = X[:,range(50) + range(190,240)]
+#for n_comp in range(2,6):
+
+#X = np.copy(mut_gene_high_mat)
+X = mut_gene_high_mat[:,range(50) + range(188,238)]
 nmf = NMF(n_components=n_comp)
 
 W = nmf.fit_transform(X)
@@ -59,8 +58,8 @@ pat_sig = H.argmax(axis = 0)
 pat_clust_mat = X[:,pat_sig.argsort()]
 pat_clust = H[:,pat_sig.argsort()]
 
-plt.figure()
-plt.imshow(W,cmap = cm.gray_r)
+#    plt.figure()
+#    plt.imshow(W,cmap = cm.gray_r)
 plt.figure()
 plt.imshow(H,cmap = cm.gray_r)
 #plt.figure()
@@ -68,7 +67,8 @@ plt.imshow(H,cmap = cm.gray_r)
 #plt.figure()
 #plt.imshow(pat_clust_mat,cmap = cm.gray_r)
 
-pat_sig
+print gene_bmi_mut_high.index[W[:,0].argsort()[::-1][:5]]
+print pat_sig
 
 #%%
 
@@ -78,6 +78,6 @@ plt.bar(range(17740), W[:,0])
 for i in range(1,W.shape[1]):
     plt.subplot(row_plt,col_plt,i+1)
     plt.bar(range(17740),W[:,i])
-plt.suptitle('Mutational Signatures; n = ' + str(n_comp))
+    plt.suptitle('Mutational Signatures; n = ' + str(n_comp))
 
 
